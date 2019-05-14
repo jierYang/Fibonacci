5d3969fa64db687ce0e55f54ba2181c961727d14
Yang Jie <jiyang@thoughtworks.com>
2019-05-14 15:37:48 +0800

    [Jane] implement input 1 return 1.

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        public static IList<int> GetFibonacciSequence(int number)
+        {
+            var result = new List<int>();
+            
+            result.Add(number);
+
+            return result;
+        }
+    }
+}
---
+++ FibonacciTest/FibonacciTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace FibonacciTest
+{
+    public class FibonacciTest
+    {
+        [Fact]
+        public void Input1ShouldGet1()
+        {
+            var number = 1;
+
+            var result = Fibonacci.Fibonacci.GetFibonacciSequence(number);
+
+            Assert.Equal(result[0], 1);
+        }
+    }
+}
