9505816f78e3a38d715c118b086cc0a5e162c436
Yang Jie <jiyang@thoughtworks.com>
2019-05-14 15:43:29 +0800

    [Jane] implement input 2 return 1.

--- Fibonacci/Fibonacci.cs
+++ Fibonacci/Fibonacci.cs
@@ -8,8 +8,18 @@ namespace Fibonacci
         public static IList<int> GetFibonacciSequence(int number)
         {
             var result = new List<int>();
-            
-            result.Add(number);
+
+            if (number < 1)
+            {
+                return null;
+            }
+
+            result.Add(1);
+
+            if(number.Equals(2))
+            {
+                result.Add(1);
+            }
 
             return result;
         }
--- FibonacciTest/FibonacciTest.cs
+++ FibonacciTest/FibonacciTest.cs
@@ -13,5 +13,15 @@ namespace FibonacciTest
 
             Assert.Equal(result[0], 1);
         }
+        
+        [Fact]
+        public void Input2ShouldGet1()
+        {
+            var number = 2;
+
+            var result = Fibonacci.Fibonacci.GetFibonacciSequence(number);
+
+            Assert.Equal(result[1], 1);
+        }
     }
 }
